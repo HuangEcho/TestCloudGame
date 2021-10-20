1c31343ddee2f0f09286b8e53ea949bf5d2978e1
huangyaxing <huangyaxing@onething.net>
2021-10-20 18:56:49 +0800

    add some test case

--- service_rtc_test/test_service_rtc.py
+++ service_rtc_test/test_service_rtc.py
@@ -4,19 +4,55 @@
 import pytest
 import yaml
 import requests
+import json
 from dependent.env_self import Env
+
+yaml_file = "../dependent/env/env.yaml"
 
 
 class TestServiceRTC(object):
-    def __init__(self):
+    @pytest.fixture()
+    def driver(self):
         env = Env()
-        env.get_env_info()
-        self.port = env.service_rtc["port"]
-        self.remote_ip = env.service_rtc["remote_ip"]
+
+        env.get_env_info(yaml_file)
+        return env.service_rtc
 
     # method: GET
-    def test_peer_init(self):
-        url = "http://{0}:{1}/peer/init".format(self.remote_ip, self.port)
+    # @pytest.mark.skip(reason="temporarily not running")
+    def test_peer_init(self, driver):
+        url = "http://{0}:{1}/peer/init".format(driver["remote_ip"], driver["port"])
         response = requests.get(url)
-        assert response.code == 200
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+        assert check_response["message"] == "ok"
+        assert "peerId" in check_response["data"]
+        driver["peer_id"] = check_response["data"]["peerId"]
+
+    # method GET
+    def test_session_create(self, driver):
+        url = "http://{0}:{1}/session/create".format(driver["remote_ip"], driver["port"])
+        header = {"userid": "123", "appid": "test"}
+        response = requests.get(url, headers=header)
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+        assert check_response["message"] == "ok"
+        assert "token" in check_response["data"]
+        driver["token"] = check_response["data"]["token"]
+
+    def connection_allocate_v2(self, driver):
+        # 固定了一些信息，具体修改的地方，我还没有很了解，后续了解后再修改
+        url = "http://{0}:{1}/connection/allocate/v2?channel_id=5&package_name=jp.ogapee.onscripter.release&version_code=20210831".format(driver["remote_ip"], driver["port"])
+        header = {"Session-Token": driver["token"], "Peer-Id": driver["peer_id"]}
+        response = requests.get(url, headers=header)
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+
+
+
+if __name__ == '__main__':
+    pytest.main(["-s", "test_service_rtc.py"])
+
 
