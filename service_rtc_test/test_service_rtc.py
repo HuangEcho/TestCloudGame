9c0d6c5a9d8986286e7ed369cd875b2b4b5e69cf
huangyaxing <huangyaxing@onething.net>
2021-10-25 18:03:58 +0800

    update test case

--- service_rtc_test/test_service_rtc.py
+++ service_rtc_test/test_service_rtc.py
@@ -2,7 +2,6 @@
 # create_time: 2021/10/19 8:19 下午
 
 import pytest
-import yaml
 import requests
 import json
 import logging
@@ -26,9 +25,21 @@ class TestServiceRTC(object):
                 url = "http://{0}:{1}/session/create".format(driver["remote_ip"], driver["port"])
                 header = {"userid": "123", "appid": "test"}
                 driver["token"] = json.loads(requests.get(url, headers=header).text)["data"]["token"]
+
+                url = "http://{0}:{1}/admin/connection/get".format(driver["remote_ip"], driver["port"])
+                header = {"Session-Token": driver["token"]}
+                data = {"sdk_version": "test_1.0.0", "sdk_type": "android", "os": "android 10"}
+                # 这里的结果还需要确认一下
+                driver["node_id"] = json.loads(requests.post(url, data=data, headers=header))["data"]["nodeId"]
+
         except Exception as E:
             logging.debug("error is {0}".format(E))
-        return driver
+        yield driver
+        # close session
+        if "token" in driver:
+            url = "http://{0}:{1}/session/close".format(driver["remote_ip"], driver["port"])
+            header = {"Session-Token": driver["token"]}
+            requests.get(url, headers=header)
 
     # method: GET
     # @pytest.mark.skip(reason="temporarily not running")
@@ -41,7 +52,6 @@ class TestServiceRTC(object):
         assert check_response["code"] == 0
         assert check_response["message"] == "ok"
         assert "peerId" in check_response["data"]
-        # driver["peer_id"] = check_response["data"]["peerId"]
 
     # method GET
     def test_session_create(self, driver):
@@ -54,7 +64,6 @@ class TestServiceRTC(object):
         assert check_response["code"] == 0
         assert check_response["message"] == "ok"
         assert "token" in check_response["data"]
-        # driver["token"] = check_response["data"]["token"]
 
     # method GET
     def test_connection_allocate_v2(self, driver):
@@ -81,14 +90,76 @@ class TestServiceRTC(object):
         check_response = json.loads(response.text)
         assert check_response["code"] == 0
 
+    # 自己生成一个sessionid，再去close
     def test_session_close(self, driver):
+        # 获取一个token
         url = "http://{0}:{1}/session/create".format(driver["remote_ip"], driver["port"])
         header = {"userid": "123", "appid": "test"}
         token = json.loads(requests.get(url, headers=header).text)["data"]["token"]
+
         url = "http://{0}:{1}/session/close".format(driver["remote_ip"], driver["port"])
         header = {"Session-Token": token}
         response = requests.get(url, headers=header)
-        
+        logging.debug("response status code is {0}, text is {1} ".format(response.status_code, response.text))
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+
+    # method POST
+    def test_sdk_info(self, driver):
+        url = "http://{0}:{1}/sdk/info".format(driver["remote_ip"], driver["port"])
+        header = {"Session-Token": driver["token"]}
+        data = {"sdk_version": "test_1.0.0", "sdk_type": "android", "os": "android 10"}
+        response = requests.post(url, data=data, headers=header)
+        logging.debug("response status code is {0}, text is {1} ".format(response.status_code, response.text))
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+
+    # method GET
+    def test_admin_connection_get(self, driver):
+        url = "http://{0}:{1}/admin/connection/get".format(driver["remote_ip"], driver["port"])
+        header = {"Session-Token": driver["token"]}
+        data = {"sdk_version": "test_1.0.0", "sdk_type": "android", "os": "android 10"}
+        response = requests.post(url, data=data, headers=header)
+        logging.debug("response status code is {0}, text is {1} ".format(response.status_code, response.text))
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+
+    # method POST
+    def test_admin_agent_msg_ready(self, driver):
+        url = "http://{0}:{1}/admin/agent/msg-ready".format(driver["remote_ip"], driver["port"])
+        header = {"Session-Token": driver["token"]}
+        data = {"node_id": driver["node_id"], "data": {"type": "ready", "session_id": "${token}", "reason": "test", "code": 1}}
+        response = requests.post(url, data=data, headers=header)
+        logging.debug("response status code is {0}, text is {1} ".format(response.status_code, response.text))
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+
+    # method POST
+    def test_admin_agent_msg_config(self, driver):
+        url = "http://{0}:{1}/admin/agent/msg-config".format(driver["remote_ip"], driver["port"])
+        header = {"Session-Token": driver["token"]}
+        data = {"node_id": driver["node_id"], "data": {"type": "config", "session_id": "${token}", "reason": "test", "code": 1}}
+        response = requests.post(url, data=data, headers=header)
+        logging.debug("response status code is {0}, text is {1} ".format(response.status_code, response.text))
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
+
+    # method POST
+    def test_admin_agent_msg_init_config(self, driver):
+        url = "http://{0}:{1}/admin/agent/msg-init-config".format(driver["remote_ip"], driver["port"])
+        header = {"Session-Token": driver["token"]}
+        data = {"node_id": driver["node_id"],
+                "data": {"type": "config", "session_id": "${token}", "reason": "test", "code": 1}}
+        response = requests.post(url, data=data, headers=header)
+        logging.debug("response status code is {0}, text is {1} ".format(response.status_code, response.text))
+        assert response.status_code == 200
+        check_response = json.loads(response.text)
+        assert check_response["code"] == 0
 
 
 if __name__ == '__main__':
